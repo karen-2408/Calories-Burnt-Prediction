8b2a78936e8b6f4051b3c4a2b3775d4199ffa9fb
Karen Menghrajani <menghraj@ualberta.ca>
2025-08-14 23:44:01 -0600

    Update calorie.py

--- calorie.py
+++ calorie.py
@@ -25,3 +25,18 @@ df.info()
 
 # check the descriptive statistical measures of the data.
 df.describe()
+
+# to check the data analysis by seeing the scatterplot
+sb.scatterplot(x='Height', y='Weight', data=df) 
+plt.show()
+#shows a linear relationship 
+
+features = ['Age', 'Height', 'Weight', 'Duration']
+plt.subplots(figsize=(15, 10))
+for i, col in enumerate(features):
+    plt.subplot(2, 2, i + 1)
+    x = df.sample(1000)
+    sb.scatterplot(x=col, y='Calories', data=x)
+plt.tight_layout()
+plt.show()
+# higher is the duration of the workout higher will be the calories burnt.
