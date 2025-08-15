13921e077a910dd293513c8b810d21090a7ac027
Karen Menghrajani <menghraj@ualberta.ca>
2025-08-14 23:40:11 -0600

    Create calorie.py

---
+++ calorie.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn import metrics
+from sklearn.svm import SVC
+from xgboost import XGBRegressor
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.ensemble import RandomForestRegressor
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# load the dataset into the panda's data frame
+df = pd.read_csv('calories.csv')
+df.head()
+
+# check the size of the dataset.
+df.shape
+
+# check which column of the dataset contains which type of data.
+df.info()
+
+# check the descriptive statistical measures of the data.
+df.describe()
